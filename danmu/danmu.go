853e3f2273d81f527e24a19a8710dd3b8bf02eb4
shynome <shynome@gmail.com>
2023-08-13 20:31:15 +0800

    node 可以直接用 stdin 作为输入

--- danmu/danmu.go
+++ danmu/danmu.go
@@ -7,23 +7,12 @@ import (
 	"io"
 	"os"
 	"os/exec"
-	"path/filepath"
-
-	"github.com/lainio/err2/try"
 )
 
 //go:generate esbuild --bundle --platform=node --outdir=dist --minify danmu.mjs
 
 //go:embed dist/danmu.js
 var danmujs []byte
-
-var f string
-
-func init() {
-	dir := try.To1(os.MkdirTemp("", "bilive-oauth2"))
-	f = filepath.Join(dir, "danmu.js")
-	try.To(os.WriteFile(f, danmujs, os.ModePerm))
-}
 
 func Connect(room, bilipage string) (r *bufio.Reader, cmd *exec.Cmd) {
 	cmd = exec.Command("node", "-", room, bilipage)
