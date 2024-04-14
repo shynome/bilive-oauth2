62535b3f7296265801bbd4299f6ca03a08306168
shynome <shynome@gmail.com>
2024-04-14 09:58:49 +0800

    移除不再使用的 /ws-info 接口

--- cmd/bilive-auth/bilibili.go
+++ cmd/bilive-auth/bilibili.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"context"
 	"crypto/ed25519"
 	"fmt"
-	"net/http"
-	"time"
 
 	"github.com/golang-jwt/jwt"
 	echojwt "github.com/labstack/echo-jwt/v4"
@@ -40,23 +37,6 @@ func registerBilibiliApi(e *echo.Group, privateKey ed25519.PrivateKey, bclient *
 			return token, nil
 		},
 	}))
-
-	e.Any("/ws-info", func(c echo.Context) (err error) {
-		defer err0.Then(&err, nil, func() {
-			err = echo.NewHTTPError(400, err.Error())
-		})
-		ctx := c.Request().Context()
-		ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
-		defer cancel()
-		IDCode := c.QueryParam("IDCode")
-		if IDCode == "" {
-			return c.String(400, "require query param: IDCode")
-		}
-		app := try.To1(bclient.Open(ctx, appid, IDCode))
-		defer app.Close()
-		info := app.Info().WebsocketInfo
-		return c.JSON(http.StatusOK, info)
-	})
 
 	e.Any("/ws-info-keep", func(c echo.Context) (err error) {
 		defer err0.Then(&err, nil, nil)
